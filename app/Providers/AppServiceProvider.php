ed1968bc0160184a22125dafac6de79f65f48f3d
huychuongcr7 <daohuychuong97@gmail.com>
2020-06-21 13:18:26 +0700

    fix https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 use App;
 
@@ -38,6 +39,9 @@ class AppServiceProvider extends ServiceProvider
             \App\Services\PostServiceInterface::class,
             \App\Services\PostService::class
         );
+        if (env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 
     /**
@@ -45,10 +49,13 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
         App::bind('Helper', function() {
             return new \App\Helper\Helper;
         });
+        if (env('REDIRECT_HTTPS')) {
+            $url->formatScheme('https');
+        }
     }
 }
